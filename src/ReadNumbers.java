e94bf6a1afa7dfc789557a7e621217df2d26d02b
Toshiba <pcursuri@gmail.com>
2015-02-21 11:29:09 +0200

    Play649

---
+++ src/ReadNumbers.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Toshiba on 21.02.2015.
+ */
+public class ReadNumbers {
+public int[] readSixNumbers(){
+        int[] readNumbers = {1,10,20,30,40,49};
+        //System.out.println("Write six numbers distinc between 1 to 49, separet with ',': ");
+        return readNumbers;
+}
+}
